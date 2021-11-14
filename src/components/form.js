858b06ad5f75b4b59f99cc9c2cd91d5f15d61c35
godwin opara <godwinopara62@yahoo.com>
2021-11-14 15:36:41 +0400

    added a function to the input that displays error messages when the input field are not filed properly or empty

--- src/components/form.js
+++ src/components/form.js
@@ -10,13 +10,15 @@ class Form extends React.Component {
       lastName: "",
       email: "",
       password: "",
-      input: "",
+      error: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
+    // Get the type of input
     const inputType = e.target.name;
     const value = e.target.value;
     switch (inputType) {
@@ -34,6 +36,52 @@ class Form extends React.Component {
         break;
       default:
         return;
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    // copy the input state from this.state
+
+    const inputsState = { ...this.state };
+
+    // get the keys from the inputsState
+    const inputValue = Object.keys(inputsState);
+
+    //Loop through the keys and display the error message
+    // for input the are empty
+
+    inputValue.forEach((key) => {
+      const inputName = this.state[key];
+      if (inputName === "") {
+        // display the error message
+        const errorMessages = document.querySelectorAll(`.${key}`);
+        errorMessages.forEach((msg) => {
+          msg.classList.add("show-error");
+        });
+      } else {
+        // remove the error message
+        const errorMessages = document.querySelectorAll(`.${key}`);
+        errorMessages.forEach((msg) => {
+          msg.classList.remove("show-error");
+        });
+        return;
+      }
+    });
+
+    // check if all the input field are not empty
+    const emptyInput = inputValue.filter((key) => {
+      return inputsState[key] === "";
+    });
+
+    // if the input field are properly filled and not empty
+    // clear the input field
+    if (emptyInput.length === 0) {
+      Object.keys(inputsState).forEach((key) => {
+        inputsState[key] = "";
+      });
+      this.setState({ ...inputsState });
     }
   }
 
@@ -71,17 +119,22 @@ class Form extends React.Component {
 
     return (
       <>
-        <form className="form">
+        <form className="form" onSubmit={this.handleSubmit}>
           {formInputType.map((input) => {
             return (
-              <Input
-                key={input.id}
-                type={input.type}
-                placeholder={input.placeholder}
-                value={input.value}
-                onChange={this.handleChange}
-                name={input.name}
-              />
+              <div key={input.id} className="input-wrapper">
+                <Input
+                  type={input.type}
+                  placeholder={input.placeholder}
+                  value={input.value}
+                  onChange={this.handleChange}
+                  name={input.name}
+                />
+                <div className={`error-img ${input.name}`}></div>
+                <p
+                  className={`error-message ${input.name}`}
+                >{`${input.placeholder} can't be empty`}</p>
+              </div>
             );
           })}
           <button type="submit">Clam your free trial</button>
